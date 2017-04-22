780c0af14aa18c1bdcb2cd25caf722ebe6e51dc4
Stefan Dietz <sdvg@users.noreply.github.com>
2017-04-22 16:45:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Elastify Eurostat
+
+> Fetches data from the Eurostat API and persists it into an Elasticsearch database.
+
+## Prerequisites
+
+Node 6 or later with NPM.  
+See <https://nodejs.org/en/download/package-manager/> or <https://nodejs.org/en/download/current/>.
+
+## Usage
+
+1. Create the configuration file `.elastify-eurostatrc`, use `.elastify-eurostatrc-example` as a template.
+2. Add your Elasticsearch credentials to the configuration.
+3. Install Node dependencies with `$ npm install`.
+4. Run the script with `$ npm start`.
+
---
+++ index.js
@@ -0,0 +1,51 @@
+const fetch = require('node-fetch');
+const elasticsearch = require('elasticsearch');
+const JSONstat = require('jsonstat');
+const config = require('rc')('elastify-eurostat');
+
+const client = new elasticsearch.Client({
+  host: config.elasticHost,
+  log: config.elasticLog
+});
+
+const getDateString = () => {
+  const now = new Date();
+  const pad = num => (num < 10 ? '0' : '') + num;
+
+  return `${now.getFullYear()}-${pad(now.getMonth() + 1)}-${pad(now.getDay())}`;
+};
+
+const persistRow = row => {
+  const document = {
+    value: row.value,
+    time: row.time,
+    geo: row.geo,
+    sex: row.sex,
+  };
+
+  client.index({
+    index: `${config.elasticIndexPrefix}${getDateString()}`,
+    type: config.elasticType,
+    body: document
+  }).then(
+    () => console.log('Document persisted.'),
+    error => console.log('Error persisting document:', error)
+  );
+};
+
+const apiUri = 'https://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/migr_asyappctza?citizen=TOTAL&sex=F&sex=M&sex=UNK&precision=1&unit=PER&age=TOTAL&asyl_app=ASY_APP';
+
+fetch(apiUri)
+  .then(
+    res => res.json(),
+    error => console.log('Error fetching data from Eurostat:', error)
+  )
+  .then(data => {
+    const table = JSONstat(data)
+      .Dataset(0)
+      .toTable({ type : 'arrobj' });
+
+    table
+      .filter(row => row.geo !== 'Total')
+      .forEach(persistRow);
+  });
